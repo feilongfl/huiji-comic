0874a8b13e4f423f98a3ac3cab07432c68de07b1
feilong <614183595@qq.com>
2016-08-14 11:42:39 +0800

    修改：     down.fish

--- down.fish
+++ down.fish
@@ -1,7 +1,3 @@
 #!/usr/bin/env fish
 
-function get_pages
-
-end
-
-
+for i in (ls menu);echo $i;mkdir -p ./commic/$i;cat ./menu/$i >./commic/$i/list.txt;for m in (cat ./commic/$i/list.txt | grep 'shinmangaplayer');echo $m;set name (echo $m | sed -r 's/(.*)\t(.*)/\1/');set url (echo $m | sed -r 's/(.*)\t(.*)/\2/' | sed -r 's/index\.html/smp_cfg.json/');echo $name;echo $url;mkdir -p ./commic/$i/$name;wget -O ./commic/$i/$name/pic.json $url;for j in (jq '.pages.page[]' ./commic/$i/$name/pic.json | sed -r 's/"//g');set pic_url (echo $url | sed -r 's/index\.html//')$j;echo $pic_url;cd ./commic/$i/$name/;pwd;wget $pic_url;cd -;end;end;end
